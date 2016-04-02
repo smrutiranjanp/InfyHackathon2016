d76a97a15088bfc519a3ce9a0d445b747f9a560c
Choudhury Smrutiranjan Parida <happy.synergy@yahoo.com>
2016-04-02 19:21:29 +0530

    OTP authentication code
    OTP authentication code sending code using way2sms api

---
+++ SendSMSASP.Net/VB.aspx.vb
@@ -0,0 +1,20 @@
+﻿Imports ASPSnippets.SmsAPI
+Imports System.Collections.Generic
+Imports System.Linq
+Partial Class VB
+    Inherits System.Web.UI.Page
+    Protected Sub btnSend_Click(sender As Object, e As EventArgs)
+        SMS.APIType = SMSGateway.Site2SMS
+        SMS.MashapeKey = "<Mashape API Key>"
+        SMS.Username = txtNumber.Text.Trim()
+        SMS.Password = txtPassword.Text.Trim()
+        If txtRecipientNumber.Text.Trim().IndexOf(",") = -1 Then
+            'Single SMS
+            SMS.SendSms(txtRecipientNumber.Text.Trim(), txtMessage.Text.Trim())
+        Else
+            'Multiple SMS
+            Dim numbers As List(Of String) = txtRecipientNumber.Text.Trim().Split(","c).ToList()
+            SMS.SendSms(numbers, txtMessage.Text.Trim())
+        End If
+    End Sub
+End Class
